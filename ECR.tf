49e630faf13aa1d382ea3e3f4281f3814308f4f6
David Vanyan <101475933+DVanyan@users.noreply.github.com>
2023-04-05 18:55:14 +0400

    Delete ECR.tf

--- ECR.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "team2-ECR" {
-  name                 = "my-ecr-repo"
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-#dgdjhgfdjhfg
